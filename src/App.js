91e9a2787a7d8b69232cb1530d5a684ef2b6ee13
motografa <camilaconsiglio@gmail.com>
2019-10-16 19:57:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// useEffect substitui o componentDidMount, componentDidUpdate e componentWillUnmount
+
+// Com hooks a gente praticamente para de usar classes
+// passa a exportar função
+export default function App(){
+
+const [repositories, setrepositories] = useState([])
+
+  useEffect(async() => {
+    const response = await fetch(
+      'http://api.github.com/users/motografa/repos')
+      const data = await response.json();
+
+      setrepositories(data);
+  }, []) //useEffect só vai executar se a variavel dentro do [] mudar. Array vazio não executa mais de uma vez.
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Você tem ${filtered.length} favoritos`
+  }, [repositories]); //Dispara toda vez que repositories mudar
+
+
+function handleFavorite(id){
+  const newRepositories = repositories.map(repo =>{
+    return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
+  })
+
+  setrepositories(newRepositories);
 }
 
-export default App;
+  return (
+      <ul>{repositories.map(repo =>(
+        <li key={repo.id}>
+        {repo.name}
+        {repo.favorite && <span style={{color: 'red', padding:'0 0 0 5px'}}> Favorito </span>}
+        <button onClick={() => handleFavorite(repo.id)} >Favoritar</button>
+        </li>
+      ))}</ul>
+  );
+}
